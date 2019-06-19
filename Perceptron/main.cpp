97b7c9ed4f414d4ec54cb01b7ec24c9a8780233f
iagows <el.iagows@gmail.com>
2019-06-19 11:26:34 -0300

    calculating

--- Perceptron/main.cpp
+++ Perceptron/main.cpp
@@ -21,27 +21,66 @@ int main(int argc, char *argv[])
     DataLoader dataLoader;
     dataLoader.load(fileName);
 
-    QList<Info> inputs = dataLoader.getData();
+    QList<Info> lines = dataLoader.getData();
 
     QList<Neurone> brain;
 
-    for (Info info : inputs) {
-        QList<double> in = info.getInput();
-        QList<double> out = info.getOutput();
+    int inLen = lines.at(0).getInput().size();
+    int outLen = lines.at(0).getOutput().size();
 
-        for (double d : in) {
-            Neurone neurone;
-            neurone.setValue(d);
-            QList<double> w;
 
-            for (double dd : out) {
-                qint64 rand = QRandomGenerator64::global()->generate() & std::numeric_limits<qint64>::max();
+    for (int i = 0; i < inLen; ++i) {
+        Neurone n;
+        QList<double> w;
 
-                w.append(rand);
+        for (int o = 0; o < outLen; ++o) {
+            qint64 rand = QRandomGenerator64::global()->generate() & std::numeric_limits<qint64>::max();
+            w.append(rand);
+        }
+
+        n.setWeights(w);
+        brain.append(n);
+    }
+
+    //neuronio de limiar
+    {
+        Neurone x0;
+        x0.setValue(1);
+        QList<double> w;
+
+        for (int i = 0; i < outLen; ++i) {
+            w.append(0);
+        }
+
+        x0.setWeights(w);
+        brain.append(x0);
+    }
+
+    bool done = false;
+
+    while (!done) {
+        for (int i = 0, size = lines.size(); i < size; ++i) {
+            Info line = lines.at(i);
+            QList<double> input = line.getInput();
+            QList<double> output = line.getOutput();
+
+            for (int out = 0; out < output.size(); ++out) {
+                for (int in = 0; in < input.size(); ++in) {
+                    Neurone n = brain.at(in);
+                    n.setValue(input.at(i));
+                }
+
+                //calcular e comparar com output.at(out)
+                double net = 0;
+
+                for (Neurone n : brain) {
+
+                }
             }
 
-            neurone.setWeights(w);
         }
+
+        break;
     }
 
     return a.exec();
